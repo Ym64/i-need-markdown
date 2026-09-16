a9b7dedb5ed62615954de07077e41a8de5de00cd
M64DiamondStar <59317305+M64DiamondStar@users.noreply.github.com>
2026-09-16 11:28:04 +0200

    Add simple spacing in output html

--- src/renderer.js
+++ src/renderer.js
@@ -5,48 +5,46 @@ export class Renderer {
         switch (node.type) {
 
             case "DOCUMENT":
-                return node.children
-                    .map(child => this.render(child))
-                    .join("\n");
+                return `<div class="inmd-container">\n    ${node.children.map(child => this.render(child)).join("\n    ")}\n</div>`;
 
             case "HEADER":
-                return `<h${node.level}>${node.children.map(c => this.render(c)).join("")}</h${node.level}>`;
+                return `<h${node.level} class="inmd-header-${node.level}">${node.children.map(c => this.render(c)).join("")}</h${node.level}>`;
 
             case "PARAGRAPH":
-                return `<p>${node.children.map(c => this.render(c)).join("")}</p>`;
+                return `<p class="inmd-paragraph">${node.children.map(c => this.render(c)).join("")}</p>`;
 
             case "TEXT":
                 return node.value;
 
             case "BOLD":
-                return `<strong>${node.children.map(c => this.render(c)).join("")}</strong>`
+                return `<strong class="inmd-bold">${node.children.map(c => this.render(c)).join("")}</strong>`
 
             case "ITALIC":
-                return `<i>${node.children.map(c => this.render(c)).join("")}</i>`
+                return `<i class="inmd-italic">${node.children.map(c => this.render(c)).join("")}</i>`
 
             case "UNDERLINE":
-                return `<u>${node.children.map(c => this.render(c)).join("")}</u>`
+                return `<u class="inmd-underline">${node.children.map(c => this.render(c)).join("")}</u>`
 
             case "STRIKE_THROUGH":
-                return `<s>${node.children.map(c => this.render(c)).join("")}</s>`
+                return `<s class="inmd-strike-through">${node.children.map(c => this.render(c)).join("")}</s>`
 
             case "HORIZONTAL_RULE":
-                return `<hr>`
+                return `<hr class="inmd-horizontal-rule">`
 
             case "HYPERLINK":
-                return `<a href="${node.link}">${node.children.map(c => this.render(c)).join("")}</a>`
+                return `<a class="inmd-hyperlink" href="${node.link}">${node.children.map(c => this.render(c)).join("")}</a>`
 
             case "INLINE_CODE":
-                return `<span class="inline-code">${node.value}</span>`
+                return `<code class="inmd-inline-code">${node.value}</code>`
 
             case "UNORDERED_LIST":
-                return `<ul>\n${node.children.map(c => this.render(c)).join("\n")}\n</ul>`;
+                return `<ul class="inmd-unordered-list">\n    ${node.children.map(c => this.render(c)).join("\n    ")}\n    </ul>`;
 
             case "ORDERED_LIST":
-                return `<ol start="${node.start}">\n${node.children.map(c => this.render(c)).join("\n")}\n</ol>`;
+                return `<ol class="inmd-ordered-list" start="${node.start}">\n    ${node.children.map(c => this.render(c)).join("\n    ")}\n    </ol>`;
 
             case "LIST_ITEM":
-                return `<li>${node.children.map(c => this.render(c)).join("")}</li>`;
+                return `<li class="inmd-list-item">${node.children.map(c => this.render(c)).join("")}</li>`;
 
             default:
                 throw new Error(`Unknown node type: ${node.type}`);
